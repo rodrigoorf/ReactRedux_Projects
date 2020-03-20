ece90b6284d3869948ad8c150e1774214390b400
Rodrigo Freire <rodrigo.freire@bradesco.com.br>
2020-03-20 15:52:37 -0300

    Context

---
+++ translate/src/contexts/LanguageContext.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    }
+
+    render(){
+        return (
+            // eslint-disable-next-line no-undef
+            <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export default Context;
